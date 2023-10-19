1882165eb9b957d250d9b542afb2852ec0d758d8
unknown <Laboratorio@OV68PCALU-L1118.alumnos.cst>
2023-10-19 16:11:46 -0300

    Semana 9 GPS

--- app/src/main/java/com/example/semana9gps/MainActivity.java
+++ app/src/main/java/com/example/semana9gps/MainActivity.java
@@ -2,7 +2,6 @@ package com.example.semana9gps;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
-
 import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
@@ -16,15 +15,15 @@ public class MainActivity extends AppCompatActivity {
 
     private LocationManager ubicacion;
     TextView longitud, latitud;
-    @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         localizacion();
     }
 
-    private void localizacion(){
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+    private void localizacion() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this, new String[]{
                     Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION
             }, 1000);
@@ -32,14 +31,18 @@ public class MainActivity extends AppCompatActivity {
         longitud = findViewById(R.id.tvLongitud);
         latitud = findViewById(R.id.tvLatitud);
 
-        LocationManager ubicacion = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        ubicacion = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
 
         Location location = ubicacion.getLastKnownLocation(LocationManager.GPS_PROVIDER);
 
-        Log.d("Longitud", String.valueOf(location.getLongitude()));
-        Log.d("Latitud", String.valueOf(location.getLatitude()));
+        if (location != null) {
+            Log.d("Longitud", String.valueOf(location.getLongitude()));
+            Log.d("Latitud", String.valueOf(location.getLatitude()));
 
-        longitud.setText("Longitud" + String.valueOf(location.getLongitude()));
-        latitud.setText("Latitud" + String.valueOf(location.getLatitude()));
+            longitud.setText("Longitud: " + String.valueOf(location.getLongitude()));
+            latitud.setText("Latitud: " + String.valueOf(location.getLatitude()));
+        } else {
+            Log.d("Error", "No se pudo obtener la ubicación actual.");
+        }
     }
 }
